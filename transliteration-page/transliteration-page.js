a6308a68480a37132283ba540ed74cbf2f6a8c6b
dadadav1s <88556637+neodavis@users.noreply.github.com>
2023-10-11 00:32:57 +0300

    [feature] [3] Initial implementation

---
+++ transliteration-page/transliteration-page.js
@@ -0,0 +1,92 @@
+const source = document.querySelector('#area-source');
+const result = document.querySelector('#area-result');
+
+const ukrainianToEnglishTransliterationMap = {
+    'А': 'A',
+    'Б': 'B',
+    'В': 'V',
+    'Г': 'H',
+    'Ґ': 'G',
+    'Д': 'D',
+    'Е': 'E',
+    'Є': 'Ye',
+    'Ж': 'Zh',
+    'З': 'Z',
+    'И': 'Y',
+    'І': 'I',
+    'Ї': 'Yi',
+    'Й': 'Y',
+    'К': 'K',
+    'Л': 'L',
+    'М': 'M',
+    'Н': 'N',
+    'О': 'O',
+    'П': 'P',
+    'Р': 'R',
+    'С': 'S',
+    'Т': 'T',
+    'У': 'U',
+    'Ф': 'F',
+    'Х': 'Kh',
+    'Ц': 'Ts',
+    'Ч': 'Ch',
+    'Ш': 'Sh',
+    'Щ': 'Shch',
+    'Ь': '',
+    'Ю': 'Yu',
+    'Я': 'Ya',
+    'а': 'a',
+    'б': 'b',
+    'в': 'v',
+    'г': 'h',
+    'ґ': 'g',
+    'д': 'd',
+    'е': 'e',
+    'є': 'ye',
+    'ж': 'zh',
+    'з': 'z',
+    'и': 'y',
+    'і': 'i',
+    'ї': 'yi',
+    'й': 'y',
+    'к': 'k',
+    'л': 'l',
+    'м': 'm',
+    'н': 'n',
+    'о': 'o',
+    'п': 'p',
+    'р': 'r',
+    'с': 's',
+    'т': 't',
+    'у': 'u',
+    'ф': 'f',
+    'х': 'kh',
+    'ц': 'ts',
+    'ч': 'ch',
+    'ш': 'sh',
+    'щ': 'shch',
+    'ь': '',
+    'ю': 'yu',
+    'я': 'ya',
+};
+
+function handleSourceChange(input, output, transliterationMap) {
+    input.addEventListener('input', (event) => {
+        const sourceString = event.target.value;
+        const transliteratedString = getTransliteratedString(sourceString, transliterationMap);
+
+        displayTransliteratedOutput(transliteratedString, output)
+    })
+}
+
+function displayTransliteratedOutput(transliteratedString, output) {
+    output.value = transliteratedString;
+}
+
+function getTransliteratedString(sourceString, transliterateMap) {
+    return [...sourceString]
+        .map(sourceLetter => transliterateMap[sourceLetter] || sourceLetter)
+        .join('');
+}
+
+handleSourceChange(source, result, ukrainianToEnglishTransliterationMap);
