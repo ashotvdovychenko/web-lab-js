e36b217c789b82534a4b025d925c2cec178f16c8
Pagend <pariy.illya@gmail.com>
2023-10-11 18:13:12 +0300

    Add eventListener and error massage

--- rectangle-page/rectangle-page.js
+++ rectangle-page/rectangle-page.js
@@ -2,10 +2,17 @@ function calculateRectangle() {
     const length = parseFloat(document.getElementById("length").value);
     const width = parseFloat(document.getElementById("width").value);
 
-    if (isNaN(length) || isNaN(width) || length <= 0 || width <= 0) {
+    if (isNaN(length) || isNaN(width)) {
         document.getElementById("areaResult").textContent = "";
         document.getElementById("perimeterResult").textContent = "";
         document.getElementById("diagonalResult").textContent = "";
+        return;
+    }
+
+    if(length <= 0 || width <= 0){
+        document.getElementById("areaResult").textContent = "Values must be greater than 0";
+        document.getElementById("perimeterResult").textContent = "Values must be greater than 0";
+        document.getElementById("diagonalResult").textContent = "Values must be greater than 0";
         return;
     }
 
@@ -17,3 +24,6 @@ function calculateRectangle() {
     document.getElementById("perimeterResult").textContent = perimeter.toFixed(2);
     document.getElementById("diagonalResult").textContent = diagonal.toFixed(2);
 }
+
+document.getElementById("length").addEventListener("input", calculateRectangle, false);
+document.getElementById("width").addEventListener("input", calculateRectangle, false);
