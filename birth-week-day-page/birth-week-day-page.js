c9dae7436889c84e06508c99babaaaeb782ab535
Pagend <68565486+Pagendera@users.noreply.github.com>
2023-10-13 02:25:15 +0300

    Birth week day calculation page implementation
    * birth-week-day-page implementation
    
    * add page styles and code refactor
    
    * add min and max for input
    
    * remove unstable algorithm and add optimal version
    
    * Little bit refactor

---
+++ birth-week-day-page/birth-week-day-page.js
@@ -0,0 +1,32 @@
+const source = document.querySelector("#birthdate");
+const result = document.querySelector("#result");
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function handleSourceChange(source, result, daysOfWeek){
+    source.addEventListener("input", (event) => {
+        const birthdate = new Date(event.target.value);
+        const dayOfWeek = calculateDay(birthdate);
+        displayDayOfWeek(daysOfWeek, dayOfWeek, result);
+    });
+}
+
+function calculateDay(birthdate) {
+    return birthdate.getDay();
+}
+
+// Unstable algorithm
+// function calculateDay(birthdate) {
+//     const a = Math.floor((15 - birthdate.getMonth()) / 12);
+//     const y = birthdate.getFullYear() - a;
+//     const m = birthdate.getMonth() + 12 * a - 1;
+//
+//     return (birthdate.getDate() + y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400) + Math.floor((31 * m) / 12)) % 7;
+// }
+
+function displayDayOfWeek(daysOfWeek ,dayOfWeek, result){
+    result.textContent = daysOfWeek[dayOfWeek];
+}
+
+handleSourceChange(source, result, daysOfWeek);
+
+document.getElementById("birthdate").setAttribute("max", new Date().toLocaleDateString("fr-ca"));
