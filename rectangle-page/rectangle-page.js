08b50e2a85552170a85bb7f86b9978b28a3ce066
Pagend <pariy.illya@gmail.com>
2023-10-12 20:02:54 +0300

    add page styles and code refactor

--- rectangle-page/rectangle-page.js
+++ rectangle-page/rectangle-page.js
@@ -1,29 +1,39 @@
 function calculateRectangle() {
     const length = parseFloat(document.getElementById("length").value);
     const width = parseFloat(document.getElementById("width").value);
-
-    if (isNaN(length) || isNaN(width)) {
-        document.getElementById("areaResult").textContent = "";
-        document.getElementById("perimeterResult").textContent = "";
-        document.getElementById("diagonalResult").textContent = "";
-        return;
-    }
-
-    if(length <= 0 || width <= 0){
-        document.getElementById("areaResult").textContent = "Values must be greater than 0";
-        document.getElementById("perimeterResult").textContent = "Values must be greater than 0";
-        document.getElementById("diagonalResult").textContent = "Values must be greater than 0";
-        return;
-    }
-
     const area = length * width;
     const perimeter = 2 * (length + width);
     const diagonal = Math.sqrt(length ** 2 + width ** 2);
 
+    if(!validate(length, width)){
+        return;
+    }
+
+    displayRectangleCalculation(area, perimeter, diagonal);
+}
+
+function validate(length, width){
+    if (isNaN(length) || isNaN(width)) {
+        document.getElementById("areaResult").textContent = "";
+        document.getElementById("perimeterResult").textContent = "";
+        document.getElementById("diagonalResult").textContent = "";
+        document.getElementById("error-message").textContent = "";
+        return false;
+    }
+
+    if(length <= 0 || width <= 0){
+        document.getElementById("error-message").textContent = "Values must be greater than 0";
+        return false;
+    }
+
+    return true;
+}
+
+function displayRectangleCalculation(area, perimeter, diagonal){
     document.getElementById("areaResult").textContent = area.toFixed(2);
     document.getElementById("perimeterResult").textContent = perimeter.toFixed(2);
     document.getElementById("diagonalResult").textContent = diagonal.toFixed(2);
 }
 
-document.getElementById("length").addEventListener("input", calculateRectangle, false);
-document.getElementById("width").addEventListener("input", calculateRectangle, false);
+document.getElementById("length").addEventListener("input", calculateRectangle);
+document.getElementById("width").addEventListener("input", calculateRectangle);
